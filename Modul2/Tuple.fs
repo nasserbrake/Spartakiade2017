9e1e0320e24f8f9a9d617084c14bbce5c3066a58
Nasser Brake <nasser.brake@colliers.com>
2017-03-11 20:35:33 +0100

    Tuple

---
+++ Modul2/Tuple.fs
@@ -0,0 +1,32 @@
+﻿module Tuple
+
+type IntTuple = int * int
+let intTuple = 1,1
+
+type IntStringTuple = int * string // every int 'times' every string
+let intStringTuple  = 1,"string"
+
+type TripleIntTuple = int * int * int
+let tripleIntTuple = 1,2,3
+
+type Complex = float * float
+let complex  = 1.0,1.0
+
+type Composition = IntStringTuple * Complex
+let composition  = intStringTuple,complex
+
+let cFirst,cSecond = complex
+let kFirst,kSecond = composition
+
+let complexZweite = 1.0,1.0
+let complexDritte = 2.0,1.0
+
+let matchTuple c = 
+    match c with
+    | 0.0,0.0 -> "0.0,0.0"
+    | 1.0,1.0 -> "0.0,0.0"
+    | 1.0,2.0 -> "1.0,1.0"
+    | 1.0,_ -> "1.0,Irgendwas"
+    | _,_ -> "sonst"
+
+let c = matchTuple (1.0,1.0)
