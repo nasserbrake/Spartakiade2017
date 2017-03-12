6973170149152c98436f24cfd16f7d6da5dcae9c
Nasser Brake <nasser.brake@colliers.com>
2017-03-12 18:19:39 +0100

    Funktion: Parameter, Annotation.

--- Modul1/Anwendung/Function.fs
+++ Modul1/Anwendung/Function.fs
@@ -1,12 +1,24 @@
 ﻿module Function
 
-// let add1 x = x + 1
-
 // Mit Zwischenergebnis
-let add1 x = 
-    let z = x + 1
+// Add
+let add x y = 
+    let z = x + y
     z
 
 let isEven x = 
     let z = x % 2 = 0
     z
+
+let length (x:string) = 
+    x.Length
+
+let add1 x = 
+    let z = x + 1
+    z
+
+let addOneToInt x = x + 1
+let addOneToFloat x = x + 1.0
+let addMoreToString x = x + "More"
+
+let apply f x :int = f x
