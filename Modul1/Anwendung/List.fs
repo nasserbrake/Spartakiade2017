47b0e8cdf6007a82833498b7c5e053a7ef83a355
Nasser Brake <nasser.brake@colliers.com>
2017-03-15 19:59:43 +0100

    List

---
+++ Modul1/Anwendung/List.fs
@@ -0,0 +1,19 @@
+﻿module List
+
+let leer = []
+
+let list0 = 3::2::1::leer
+
+let list1 = [6;5;4]
+
+let list2 = list1 @ list0
+
+// let list3 = list0 :: list1
+
+let f l = 
+    match l with
+    | [] -> "Leere Liste"
+    | h::t -> 
+        sprintf "Erster Element ist %A und Liste hat noch %i Elemente" h (List.length t)
+
+
